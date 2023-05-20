9fa5db68306491216bbd82f6cc21ed22fe610169
Michael Al Tork <michael.altork@gmail.com>
2023-05-20 16:20:11 -0400

    add string exercises

---
+++ strings3/src/main.rs
@@ -0,0 +1,48 @@
+// strings3.rs
+// Execute `rustlings hint strings3` or use the `hint` watch subcommand for a hint.
+
+// I AM NOT DONE
+
+fn trim_me(input: &str) -> String {
+    // TODO: Remove whitespace from both ends of a string!
+    input.trim().to_string()
+}
+
+fn compose_me(input: &str) -> String {
+    let mut input = input.to_owned();
+    input.push_str(" world!");
+    input
+}
+
+fn replace_me(input: &str) -> String {
+    // TODO: Replace "cars" in the string with "balloons"!
+    input.replace("cars","balloons").to_string()
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn trim_a_string() {
+        assert_eq!(trim_me("Hello!     "), "Hello!");
+        assert_eq!(trim_me("  What's up!"), "What's up!");
+        assert_eq!(trim_me("   Hola!  "), "Hola!");
+    }
+
+    #[test]
+    fn compose_a_string() {
+        assert_eq!(compose_me("Hello"), "Hello world!");
+        assert_eq!(compose_me("Goodbye"), "Goodbye world!");
+    }
+
+    #[test]
+    fn replace_a_string() {
+        assert_eq!(replace_me("I think cars are cool"), "I think balloons are cool");
+        assert_eq!(replace_me("I love to look at cars"), "I love to look at balloons");
+    }
+}
+
+fn main() {
+    println!("Hello")
+}
